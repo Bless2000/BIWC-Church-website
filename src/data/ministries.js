bd5ff86251e40435d0d1a1e44f676f8e4b57bbe4
Bless2000 <blessamponsah8@gmail.com>
2026-02-15 02:46:37 +0000

    changing image locations

--- src/data/ministries.js
+++ src/data/ministries.js
@@ -2,27 +2,27 @@ const ministries = [
   {
     title: "Children's Ministry",
     description: "Nurturing young hearts in faith and love.",
-    image: "/src/assets/img/children2.jpg",
+    image: "/img/children2.jpg",
   },
   {
     title: "Youth Ministry",
     description: "Guiding teenagers to grow spiritually and connect with peers.",
-    image: "/src/assets/img/youth3.jpg",
+    image: "/img/youth3.jpg",
   },
   {
     title: "Men's Ministry",
     description: "Building strong men of God through fellowship and study.",
-    image: "/src/assets/img/men2.jpg",
+    image: "/img/men2.jpg",
   },
   {
     title: "Women's Ministry",
     description: "Empowering women to live out their faith and serve the community.",
-    image: "/src/assets/img/women.jpg",
+    image: "/img/women.jpg",
   },
   {
     title: "See Other Ministries",
     description: "Click on the button to the see the other ministires",
-    image: "/src/assets/img/church2.jpg",
+    image: "/img/church2.jpg",
   },
   
 ];
